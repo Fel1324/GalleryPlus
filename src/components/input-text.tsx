63ec1d103c830e218909c818ae66da8e399ca1de
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2026-04-02 20:49:02 -0300

    create input-checkbox, input-text, input-single-file and image-file-preview components

---
+++ src/components/input-text.tsx
@@ -0,0 +1,64 @@
+import { type VariantProps, tv } from "tailwind-variants";
+import Icon from "./icon";
+import Text from "./text";
+
+export const inputTextContainerVariants = tv({
+  base: "flex flex-col gap-1",
+});
+
+export const inputTextWrapperVariants = tv({
+  base: "border border-solid border-border-primary focus:border-border-active bg-transparent rounded flex items-center gap-3",
+  variants: {
+    size: {
+      md: "h-10 p-3",
+    },
+    disabled: {
+      true: "pointer-events-none",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+    disabled: false,
+  },
+});
+
+export const inputTextVariants = tv({
+  base: "bg-transparent outline-none placeholder:text-placeholder text-accent-paragraph flex-1"
+})
+
+export const inputTextIconVariants = tv({
+  base: "fill-placeholder",
+  variants: {
+    size: {
+      md: "w-6 h-6"
+    }
+  },
+  defaultVariants: {
+    size: "md"
+  }
+})
+
+interface InputTextProps
+  extends
+    VariantProps<typeof inputTextWrapperVariants>,
+    Omit<React.ComponentProps<"input">, "size" | "disabled"> {
+      icon?: React.ComponentProps<typeof Icon>["svg"],      
+      error?: React.ReactNode;
+    }
+
+export function InputText({ size, disabled, className, icon, error, ...props}: InputTextProps) {
+  return (
+    <div className={inputTextContainerVariants({className})}>
+      <div className={inputTextWrapperVariants({size, disabled})}>
+        {icon && <Icon className={inputTextIconVariants({size})} svg={icon} />}
+        <input className={inputTextVariants()} disabled={disabled as boolean} type="text" {...props} />
+      </div>
+
+      {error && (
+        <Text variant="label-small" className="text-accent-red">
+          Um erro do campo
+        </Text>
+      )}
+    </div>
+  );
+}
